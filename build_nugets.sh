ee1b962e94b5b8673458e189d5cf62d12c053f21
Lukas Tomasy <whololooo@gmail.com>
2025-07-17 22:22:15 +0200

    nuspec to projects

---
+++ build_nugets.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+# This script builds the NuGet packages for the project.
+root_dir=$(pwd)
+
+cd $root_dir/src/FlagCommander
+dotnet build -c Release
+nuget pack FlagCommander.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/
+cd $root_dir
+
+cd $root_dir/src/FlagCommanderUI
+dotnet build -c Release
+nuget pack FlagCommanderUI.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/
+cd $root_dir
+
+cd $root_dir/src/FlagCommanderUINet6
+dotnet build -c Release
+nuget pack FlagCommanderUINet6.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/ 
+cd $root_dir
+
+# TODO push nugets to nuget.org
