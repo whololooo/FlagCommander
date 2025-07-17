fe5d74014c324d9650074e8c78808c9cb642d56b
Lukas Tomasy <whololooo@gmail.com>
2025-07-17 23:59:51 +0200

    build_nugets.sh fix concurrency issue

--- build_nugets.sh
+++ build_nugets.sh
@@ -1,21 +1,27 @@
-﻿#!/bin/bash
+﻿#!/bin/zsh
 
 # This script builds the NuGet packages for the project.
 root_dir=$(pwd)
 
 cd $root_dir/src/FlagCommander
-dotnet build -c Release
-nuget pack FlagCommander.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/
+dotnet build -c Release -f net9.0
+dotnet build -c Release -f net8.0
+dotnet build -c Release -f net6.0
+nugetArgs="FlagCommander.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/"
+nuget pack $nugetArgs || mono /usr/local/bin/nuget.exe pack $nugetArgs
 cd $root_dir
 
 cd $root_dir/src/FlagCommanderUI
-dotnet build -c Release
-nuget pack FlagCommanderUI.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/
+dotnet build -c Release -f net9.0
+dotnet build -c Release -f net8.0
+nugetArgs="FlagCommanderUI.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/"
+nuget pack $nugetArgs || mono /usr/local/bin/nuget.exe pack $nugetArgs
 cd $root_dir
 
 cd $root_dir/src/FlagCommanderUINet6
-dotnet build -c Release
-nuget pack FlagCommanderUINet6.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/ 
+dotnet build -c Release -f net6.0
+nugetArgs="FlagCommanderUINet6.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $root_dir/artifacts/"
+nuget pack $nugetArgs || mono /usr/local/bin/nuget.exe pack $nugetArgs
 cd $root_dir
 
 # TODO push nugets to nuget.org
